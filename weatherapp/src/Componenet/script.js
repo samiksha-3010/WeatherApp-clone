9d34b56d858b2195b3be430a05a282de1a409dac
samiksha-3010 <samiksha30102@gmail.com>
2023-10-06 14:58:29 +0530

    done

---
+++ weatherapp/src/Componenet/script.js
@@ -0,0 +1,39 @@
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+   if(inputBox.value === ""){
+ alert("You must write Something!")
+   }
+   else{
+    let li = document.createElement("li")
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li)
+    let span = document.createElement("span")
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+   }
+   inputBox.value = ""
+   saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagname === "LI"){
+        e.target.classList.toogle("checked")
+    }
+    else if (e.target.tagname === "SPAN"){
+        e.target.parentElement.remove();
+
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");  
+}
+showTask();
+
